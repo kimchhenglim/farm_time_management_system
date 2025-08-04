5f139826ed7972343612d464b93c7fd6a615f03e
Justin Bui <buidominhquanhp1999@gmail.com>
2025-08-04 20:22:53 +0930

    Project Backend Initial Setup

---
+++ src/main/java/com/example/comp9034/Comp9034FarmProjectManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.comp9034;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Comp9034FarmProjectManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Comp9034FarmProjectManagementApplication.class, args);
+    }
+
+}
