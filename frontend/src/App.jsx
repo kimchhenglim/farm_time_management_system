541feeb930c712291704b2923b511914a1adf6f5
CHINGSIEN <102890973@student.swin.edu.au>
2025-08-19 21:58:24 +0930

    Frontend Development
    - Installed react router dom
    - Added FarmLogo.svg

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,53 +1,14 @@
-import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import axios from "axios";
+import { Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
 function App() {
-  const [count, setCount] = useState(0);
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        // Example API
-        const response = await axios.get(
-          "http://localhost:8088/The-Project/public/api/users/get/user?userInput=test"
-        );
-        setData(response.data);
-      } catch (err) {
-        setError(err.message || "Something went wrong");
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchData();
-  }, []);
-  console.log(data);
   return (
     <>
-      <div className="overflow-x-auto">
-        <table className="table">
-          {/* head */}
-          <thead>
-            <tr>
-              <th></th>
-              <th>Message</th>
-              <th>Flow</th>
-            </tr>
-          </thead>
-          <tbody>
-            {/* row 1 */}
-            <tr>
-              <th>1</th>
-              <th>{data?.message}</th>
-              <th>{data?.flow}</th>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+      <main className="h-screen w-screen overflow-y-auto bg-[#F4F6F8]">
+        <Routes>
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </main>
     </>
   );
 }
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,46 @@
 import React from "react";
-
+import Logo from "../assets/FarmLogo.svg";
 function Login() {
-  return <div></div>;
+  return (
+    <div className="text-black w-screen  h-screen flex flex-col items-center justify-center ">
+      <div className="w-[50%] h-auto shadow-[var(--custom-shadow)] rounded-lg overflow-hidden p-4 bg-white">
+        {/* logo */}
+        <div className="flex flex-col">
+          <img src={Logo} alt="farmLogo" className="w-[150px] h-[35px]" />
+          <div className="text-[#ADADAD] text-xs">
+            Log in to your account to manage everything
+          </div>
+        </div>
+        <div>
+          <form action="">
+            <div>
+              <label htmlFor="username">Username</label>
+              <input type="text" name="" id="username" />
+            </div>
+            <div>
+              <label htmlFor="password">Password</label>
+              <input type="text" name="" id="password" />
+            </div>
+            <div>
+              <div>
+                <input type="text" name="" id="" />
+                <label htmlFor=""></label>
+              </div>
+              <div>
+                <span>Forgot password?</span>
+              </div>
+            </div>
+            <button>Log In</button>
+          </form>
+          <div>
+            <span>
+              Don’t have an account? <span>Sign Up</span>
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
