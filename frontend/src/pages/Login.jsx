d24b843f2eb4e36c574633e6b096ed999b872ee3
Chingsien <102890973@student.swin.edu.au>
2025-08-19 23:05:48 +0930

    Frontend Development
    - Finished Login function

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,40 +1,85 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../assets/FarmLogo.svg";
+import Eye from "../assets/eye.svg";
 function Login() {
+  const [hidden, setHidden] = useState(true);
+
+  //submit function
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert("Login successfully!");
+  };
   return (
     <div className="text-black w-screen  h-screen flex flex-col items-center justify-center ">
-      <div className="w-[50%] h-auto shadow-[var(--custom-shadow)] rounded-lg overflow-hidden p-4 bg-white">
+      <div className="w-[25%] h-auto shadow-[var(--custom-shadow)] rounded-2xl overflow-hidden px-[40px] py-[32px] bg-white">
         {/* logo */}
         <div className="flex flex-col">
-          <img src={Logo} alt="farmLogo" className="w-[150px] h-[35px]" />
-          <div className="text-[#ADADAD] text-xs">
+          <img src={Logo} alt="farmLogo" className="w-[250px] h-auto" />
+          <div className="text-[#ADADAD] text-lg">
             Log in to your account to manage everything
           </div>
         </div>
-        <div>
-          <form action="">
-            <div>
-              <label htmlFor="username">Username</label>
-              <input type="text" name="" id="username" />
-            </div>
-            <div>
-              <label htmlFor="password">Password</label>
-              <input type="text" name="" id="password" />
-            </div>
-            <div>
-              <div>
-                <input type="text" name="" id="" />
-                <label htmlFor=""></label>
-              </div>
-              <div>
-                <span>Forgot password?</span>
+        <div className="mt-[32px]">
+          <form onSubmit={handleSubmit}>
+            <div className="flex flex-col gap-2.5">
+              <label htmlFor="username" className="font-medium text-[#566074]">
+                Username
+              </label>
+              <div className="w-full border-[2px] rounded-md flex border-[#ADADAD]">
+                <input
+                  placeholder="Type your username"
+                  type="text"
+                  name="username"
+                  id="username"
+                  className="w-[90%]  border-[#ADADAD] p-2 rounded-md outline-0"
+                />
+                <img src={Eye} alt="eye" />
               </div>
             </div>
-            <button>Log In</button>
+            <div className="flex flex-col mt-[23px] gap-2.5">
+              <label htmlFor="password" className="font-medium text-[#566074]">
+                Password
+              </label>
+              <div className="w-full border-[2px] rounded-md flex border-[#ADADAD]">
+                <input
+                  type={`${hidden ? "password" : "text"}`}
+                  name=""
+                  placeholder="Type your password"
+                  id="password"
+                  className="w-[90%]  border-[#ADADAD] p-2 rounded-md outline-0"
+                />
+                <img
+                  src={Eye}
+                  alt="eye"
+                  onClick={() => setHidden((prev) => !prev)}
+                />
+              </div>
+            </div>
+            <div className="mt-[32px] flex justify-between items-center">
+              <div className="flex items-center justify-center gap-2 text-[#ADADAD]">
+                <input
+                  type="checkbox"
+                  defaultChecked
+                  className="checkbox rounded-xs size-[18px] border-[#ADADAD]  checked:bg-red-400 checked:text-white"
+                />
+                <label htmlFor="">Remember me</label>
+              </div>
+              <div className="text-blue-500 underline">
+                <span className="font-medium">Forgot password?</span>
+              </div>
+            </div>
+            <button
+              className="w-full bg-[#16A34A] py-[14px] text-xl mt-[32px] font-bold text-white rounded-md cursor-pointer"
+              type="submit"
+            >
+              Log In
+            </button>
           </form>
-          <div>
-            <span>
-              Don’t have an account? <span>Sign Up</span>
+          <div className="mt-[32px] flex flex-col gap-[32px] items-center">
+            <div className="w-full h-[1px] bg-[#E8E8E8]"></div>
+            <span className="text-[#ADADAD]">
+              Don’t have an account?{" "}
+              <span className="text-[#16A34A] underline fot-">Sign Up</span>
             </span>
           </div>
         </div>
