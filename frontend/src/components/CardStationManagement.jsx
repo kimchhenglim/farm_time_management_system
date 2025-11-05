ab0ef3e50a634ca13da1c653dceb9a4781a170e6
CHINGSIEN <102890973@student.swin.edu.au>
2025-11-05 11:27:39 +1030

    Enhances UI/UX of station management and staff table
    Improves the user interface and user experience of the card station management component by wrapping it with a Link component, making it clickable.
    
    Enhances the StaffTable component:
    - Removes the cursor pointer from the "Export" button's parent div.
    - Adds "m" to the break time displayed in the StaffTable to denote minutes.

--- frontend/src/components/CardStationManagement.jsx
+++ frontend/src/components/CardStationManagement.jsx
@@ -7,54 +7,56 @@ function CardStationManagement({ bg, symbol, text_color, border, text, path }) {
   // handle the break
   const handleBreak = () => {};
   return (
-    <div
-      className={`cursor-pointer mt-[150px] w-[250px] h-[250px] flex flex-col  ${text_color} font-semibold text-2xl items-center justify-center rounded-[30px] ${border} bg-[${bg}] relative`}
-    >
-      {text === "Break-in" ? (
-        <div
-          className="flex items-center justify-center flex-col gap-4"
-          onClick={() => setIsClicked((prev) => !prev)}
-        >
-          {symbol}
-          <span>{text}</span>
-        </div>
-      ) : (
-        <Link
-          to={path}
-          className="flex items-center justify-center flex-col gap-4"
-        >
-          {symbol}
-          <span>{text}</span>
-        </Link>
-      )}
-
-      {/* if text="Break-in" */}
-
-      {!isClicked ? (
-        <></>
-      ) : (
-        <div
-          className={`${
-            text === "Break-in" ? "block" : "hidden"
-          } absolute top-[85%] left-[75%] border-2 border-[#16A34A] w-[200px] h-fit  rounded-lg bg-[#FBFBFB] flex flex-col text-[#566074] `}
-        >
-          <Link
-            to={`${path}/meal-break`}
-            className="p-2 cursor-pointer hover:bg-[#16A34A] hover:text-white"
+    <Link>
+      <div
+        className={`cursor-pointer mt-[150px] w-[250px] h-[250px] flex flex-col  ${text_color} font-semibold text-2xl items-center justify-center rounded-[30px] ${border} bg-[${bg}] relative`}
+      >
+        {text === "Break-in" ? (
+          <div
+            className="flex items-center justify-center flex-col gap-4"
+            onClick={() => setIsClicked((prev) => !prev)}
           >
-            Meal break
-          </Link>
-          {/* line break*/}
-          <div className="bg-black h-[1px] w-full"></div>
+            {symbol}
+            <span>{text}</span>
+          </div>
+        ) : (
           <Link
-            to={`${path}/emergency`}
-            className="p-2 cursor-pointer  hover:bg-[#16A34A] hover:text-white"
+            to={path}
+            className="flex items-center justify-center flex-col gap-4"
           >
-            Emergency
+            {symbol}
+            <span>{text}</span>
           </Link>
-        </div>
-      )}
-    </div>
+        )}
+
+        {/* if text="Break-in" */}
+
+        {!isClicked ? (
+          <></>
+        ) : (
+          <div
+            className={`${
+              text === "Break-in" ? "block" : "hidden"
+            } absolute top-[85%] left-[75%] border-2 border-[#16A34A] w-[200px] h-fit  rounded-lg bg-[#FBFBFB] flex flex-col text-[#566074] `}
+          >
+            <Link
+              to={`${path}/meal-break`}
+              className="p-2 cursor-pointer hover:bg-[#16A34A] hover:text-white"
+            >
+              Meal break
+            </Link>
+            {/* line break*/}
+            <div className="bg-black h-[1px] w-full"></div>
+            <Link
+              to={`${path}/emergency`}
+              className="p-2 cursor-pointer  hover:bg-[#16A34A] hover:text-white"
+            >
+              Emergency
+            </Link>
+          </div>
+        )}
+      </div>
+    </Link>
   );
 }
 
